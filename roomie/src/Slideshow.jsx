ea9f2ac18c3eca5969228aa556b93d65834e0597
ISIDORA ABUMOHOR <isidora.abumohor@uc.cl>
2023-11-30 18:38:41 -0300

    fixing

--- roomie/src/Slideshow.jsx
+++ roomie/src/Slideshow.jsx
@@ -6,7 +6,7 @@ import background2 from '../assets/background/background2.jpg';
 import background3 from '../assets/background/background3.jpg';
 import background4 from '../assets/background/background4.jpg';
 import background5 from '../assets/background/background5.jpg';
-import background6 from '..assets/background/background6.jpg';
+import background6 from '../assets/background/background6.jpg';
 import background7 from '../assets/background/background7.jpg';
 import background8 from '../assets/background/background8.jpg';
 
